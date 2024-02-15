41812579d63b6fe82c506848201727ed89092907
Ali-Vazife <pcvazife@gmail.com>
2024-02-15 19:43:29 +0330

    fix error handler

--- middleware/error-Handler.js
+++ middleware/error-Handler.js
@@ -4,7 +4,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof customAPIError) {
     return res.status(err.statusCode).json({ msg: err.msg })
   }
-  return res.status(err.status).json({ msg: err.message })
+  return res.status(500).json({ msg: 'Something went wrong, please try again' })
 }
 
 module.exports = errorHandlerMiddleware;
